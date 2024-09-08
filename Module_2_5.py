f54a6e8c320ac19fe667695af15092fa21946c8d
Zlyuka <zhenya0699@gmail.com>
2024-09-08 15:28:34 +0300

    1

--- Module_2_5.py
+++ Module_2_5.py
@@ -1,7 +1,12 @@
-def get_matrix(n, m, value):
+def get_matrix(n):
     matrrix = []
     for n in range(n):
         if n <= n:
             print(f'\n ')
         else:
             break
+pass
+
+n = input (int())
+get_matrix(n)
+
