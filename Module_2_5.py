5b0d00d263c32596cc1d0341b782d5280cb4632f
Zlyuka <zhenya0699@gmail.com>
2024-09-10 18:05:17 +0300

    1

--- Module_2_5.py
+++ Module_2_5.py
@@ -1,16 +1,12 @@
-def get_matrix(n):
-    matrrix = []
-    for n in range(n):
-        if n <= n:
-            matrrix.append([])
-            print(matrrix.append([]))
-        else:
-            break
-    return matrrix.append([])
-            # for m in range(m):
-            #     if
-pass
-
-n = int(input())
-get_matrix(n)
-
+def get_matrix(n,m,value):
+    matrix = []
+    for i in range(n):
+        matrix_1 = [value] * m
+        matrix.append(matrix_1)
+    return matrix
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
